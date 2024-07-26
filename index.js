9a5688f5a789783fe0238c7ec088b86fb05cbe35
Dhruva Kumar <dhr7va@gmail.com>
2024-07-26 12:58:03 +0530

    edit done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadProducts();
+});
+
 function handleFormSubmit(event) {
     event.preventDefault();
 
+    const price = event.target.price.value;
+    const prodName = event.target.name.value;
+
+    if (!price || !prodName) {
+        alert('Both fields are required');
+        return;
+    }
+
     const productDetails = {
-        price: event.target.price.value,
-        prodName: event.target.name.value
+        price: price,
+        prodName: prodName
     };
 
     axios
@@ -19,20 +31,31 @@ function displayProd(productDetails) {
     const sellerDetails = document.createElement("li");
     sellerDetails.textContent = `${productDetails.price} - ${productDetails.prodName}`;
 
-    const deletBtn = document.createElement("button");
-    deletBtn.classList.add("btn", "btn-danger");
-    deletBtn.textContent = "Delete";
-    sellerDetails.appendChild(deletBtn);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn", "btn-danger");
+    deleteBtn.textContent = "Delete";
+    sellerDetails.appendChild(deleteBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("btn", "btn-warning");
+    editBtn.textContent = "Edit";
+    sellerDetails.appendChild(editBtn);
 
     const prodList = document.querySelector("ul");
     prodList.appendChild(sellerDetails);
 
-    deletBtn.addEventListener("click", function () {
+    deleteBtn.addEventListener("click", function () {
         axios.delete(`https://crudcrud.com/api/eaaff60087234a23b055698bac69424f/sellerInfo/${productDetails._id}`)
             .then(() => {
                 prodList.removeChild(sellerDetails);
             })
             .catch((error) => console.log(error));
+    });
+
+    editBtn.addEventListener("click", function (event) {
+        prodList.removeChild(event.target.parentElement);
+        document.getElementById("price").value = productDetails.price;
+        document.getElementById("name").value = productDetails.prodName;
     });
 }
 
@@ -48,5 +71,3 @@ function loadProducts() {
 }
 
 document.querySelector('form').addEventListener('submit', handleFormSubmit);
-
-document.addEventListener('DOMContentLoaded', loadProducts);
